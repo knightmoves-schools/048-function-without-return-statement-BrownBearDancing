a988787177cdb91cba1c796c0696dff4a5501a81
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-12 13:18:04 -0600

    mustache brackets

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let logUser= function("username") {
-  console.log$(username);
+let logUser= function(username) {
+  console.log${username};
 }
-document.getElementById("result").innerHTML = logUser("username");
+document.getElementById("result").innerHTML = logUser("name");
 
 should create a function named logUser that takes one parameter username
 should log user: ${username} to the console when logUser is called
 should not return a value when logUser is called
+
 
 
