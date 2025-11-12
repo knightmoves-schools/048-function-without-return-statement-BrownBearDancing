19fc25ab689b2878821bc8f853229cb646881f1e
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-12 13:50:46 -0600

    added semicolon line 3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function logUser(username) {
   console.log ${username};
-}
-document.getElementById("result").innerHTML = logUser();
+};
+document.getElementById("result").innerHTML = logUser("aname");
 
 should create a function named logUser that takes one parameter username
 should log user: ${username} to the console when logUser is called
 should not return a value when logUser is called
+
 
 
 
