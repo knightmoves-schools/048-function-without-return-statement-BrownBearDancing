d15e114f55f964acbe4e68806cef61085392dc7d
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-12 13:44:06 -0600

    try again

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function logUser(username) {
-  console.log$;{username};
+  console.log ${username};
 }
-document.getElementById("result").innerHTML = logUser("name");
+
 
 should create a function named logUser that takes one parameter username
 should log user: ${username} to the console when logUser is called
 should not return a value when logUser is called
+
 
 
 
